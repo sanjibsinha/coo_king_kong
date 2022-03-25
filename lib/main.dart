259b8902b81f6ae0c2717775c82a2c5823968468
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-03-25 06:26:36 +0530

    First and final to fifth branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'view/coo_king_kong_app.dart';
 
+/// in this fifth branch we have followed the
+/// MVC model to apply clean coding principle
+
 void main() {
   runApp(const CooKingKongApp());
 }
